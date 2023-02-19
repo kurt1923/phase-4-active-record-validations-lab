d776c219f4ee3880418c48858db355c87f8d0471
Kurt Vermillion <kurtv0727@gmail.com>
2023-02-19 17:36:59 -0500

    rails validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def clickbait 
+        if CLICKBAIT_PATTERNS.none? do |pattern|
+            pattern.match(title)
+        end 
+        errors.add(:title, "must be clickbait")
 end
